7fa5fb492e05983e39766af95ca64087b1df49c2
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2023-11-14 20:26:53 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,58 @@
+        int[] data = new int[10];
+        for (int item : data) {
+
+        }
+public class RandomDataGenerator {
+        }
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+    }
+
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+main();
+    });
+function main() {
+        console.log("Random Number: " + item);
+    const data = [];
+    const data = generateRandomData();
+}
+    }
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+
+
+    return data;
+}
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = generateRandomData();
+
+main();
+}
+
+    const data = [];
+function generateRandomData() {
+    });
+    return data;
+
+        console.log("Random Number: " + item);
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+def generate_random_data():
+def main():
+
+
+if __name__ == "__main__":
+    return data
+
+import random
+    main()
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
